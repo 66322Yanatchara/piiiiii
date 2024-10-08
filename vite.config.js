8c2e2850e6101d71da185d2fe353c7b64743ead0
66322 Yanatchara <yanatchara.fongloy@gmail.com>
2024-10-08 17:15:30 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['src/main.jsx']
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png']
 })
+
