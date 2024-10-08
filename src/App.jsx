c0b8b6763bb98178f0f87e02ec176e2bed00854e
66322 Yanatchara <yanatchara.fongloy@gmail.com>
2024-10-08 20:10:48 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import './app.css'; // นำเข้าไฟล์ CSS
+import './App.css'; // นำเข้าไฟล์ CSS
+import './index.css';
 
 const PigGame = () => {
   const [level, setLevel] = useState(0);
